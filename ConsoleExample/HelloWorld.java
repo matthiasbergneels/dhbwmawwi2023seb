59d9561c52168d235ea97ff3fa4e9b7064eb7508
Matthias Berg-Neels <matthiasuweberg@icloud.com>
2023-12-20 22:42:41 +0100

    Console Example & JavaDoc

---
+++ ConsoleExample/HelloWorld.java
@@ -0,0 +1,14 @@
+/** lecture.chapter2.HelloWorld class as first programming example
+* @author Matthias Berg-Neels
+* @version 1.0
+* @since 1.0
+*/
+public class HelloWorld {
+
+  /** main method as starting point for program start
+  * @param args String array for parameters from the console
+  */
+  public static void main (String[] args){
+    System.out.println("Hello World!");
+  }
+}
---
+++ ConsoleExample/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"HelloWorld","l":"HelloWorld()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"HelloWorld","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
