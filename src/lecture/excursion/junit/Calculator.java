772f7c4447a01da0d7fac86047523f8f87dc03f5
Matthias Berg-Neels <matthiasuweberg@icloud.com>
2024-01-15 15:58:14 +0100

    JUnit CalculatorTest Class

--- src/lecture/excursion/junit/Calculator.java
+++ src/lecture/excursion/junit/Calculator.java
@@ -5,4 +5,12 @@ public class Calculator {
   public double add(double numberA, double numberB){
     return numberA + numberB;
   }
+
+  public double subtract(double numberA, double numberB){
+    return numberA - numberB;
+  }
+
+  public double multiply(double numberA, double numberB){
+    return numberA * numberB;
+  }
 }
--- src/lecture/excursion/junit/CalculatorTest.java
+++ src/lecture/excursion/junit/CalculatorTest.java
@@ -30,42 +30,101 @@ class CalculatorTest {
     System.out.println("Läuft EINMAL nach allen Test - aufräumen");
   }
 
-  @Test
-  void add() {
-    System.out.println("Test - add");
-    // Arrange
-    // Nothing to do
+  @Nested
+  @DisplayName("Add-Test Cases")
+  class AddTestCases {
+    @Test
+    @DisplayName("einfacher add Test")
+    @RepeatedTest(10)
+    void add() {
+      System.out.println("Test - add");
+      // Arrange
+      // Nothing to do
 
-    // Act
-    double result = testCalculator.add(10.0, 20.0);
+      // Act
+      double result = testCalculator.add(10.0, 20.0);
 
-    // Assert
-    Assertions.assertEquals(30.0, result);
+      // Assert
+      Assertions.assertEquals(30.0, result);
+    }
+
+    @Test
+    @DisplayName("add Test mit Integer Werten")
+    @Tag("UnitTest")
+    void addWithIntegerValue() {
+      System.out.println("Test - addWithIntegerValue");
+      // Arrange
+      // Nothing to do
+
+      // Act
+      double result = testCalculator.add(5, 10);
+
+      // Assert
+      Assertions.assertEquals(15.0, result);
+    }
+
+    @Test
+    @Tag("IntegrationTest")
+    @DisplayName("add Test mit kleinen Werten")
+    void addWithSmallValue() {
+      System.out.println("Test - addWithSmallValue");
+      // Arrange
+      // Nothing to do
+
+      // Act
+      double result = testCalculator.add(0.00005, 0.00001);
+
+      // Assert
+      Assertions.assertEquals(0.00006, result);
+    }
+
+    @Test
+    @Tag("IntegrationTest")
+    @DisplayName("add Test mit großen Werten")
+    void addWithBigValue() {
+      System.out.println("Test - addWithSmallValue");
+      // Arrange
+      // Nothing to do
+
+      // Act
+      double result = testCalculator.add(50000000, 50000000);
+
+      // Assert
+      Assertions.assertEquals(100000000, result);
+    }
   }
 
-  @Test
-  void addWithIntegerValue() {
-    System.out.println("Test - addWithIntegerValue");
-    // Arrange
-    // Nothing to do
+  @Nested
+  @DisplayName("Subtract-Test Cases")
+  class SubtractTestCases {
+    @Test
+    @DisplayName("einfacher subtract Test")
+    @RepeatedTest(10)
+    void subtract() {
+      System.out.println("Test - Subrtact");
+      // Arrange
+      // Nothing to do
 
-    // Act
-    double result = testCalculator.add(5, 10);
+      // Act
+      double result = testCalculator.subtract(10.0, 20.0);
 
-    // Assert
-    Assertions.assertEquals(15.0, result);
-  }
+      // Assert
+      Assertions.assertEquals(-10.0, result);
+    }
 
-  @Test
-  void addWithSmallValue() {
-    System.out.println("Test - addWithSmallValue");
-    // Arrange
-    // Nothing to do
+    @Test
+    @DisplayName("subtract Test mit Integer Werten")
+    @Tag("UnitTest")
+    void subtractWithIntegerValue() {
+      System.out.println("Test - addWithIntegerValue");
+      // Arrange
+      // Nothing to do
 
-    // Act
-    double result = testCalculator.add(0.00005, 0.00001);
+      // Act
+      double result = testCalculator.subtract(5, 10);
 
-    // Assert
-    Assertions.assertEquals(0.00006, result);
+      // Assert
+      Assertions.assertEquals(-5.0, result);
+    }
   }
 }
